ffa03660874466197d65584f913412fb80aada89
Nguyen Manh Hung <HungNMHE187081@fpt.edu.vn>
2024-10-03 17:26:36 +0700

    Add project files.

---
+++ Solution_01/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    public class MyClass
+    {
+       public void Display<T,U> (T msg, U value)
+        {
+            Console.WriteLine($"{msg} : {value}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Display<string, int> ("Hello", 10);
+
+            obj.Display<double , char> (103.5f, 'A');
+            Console.ReadLine();
+        }
+    }
+    
+}
