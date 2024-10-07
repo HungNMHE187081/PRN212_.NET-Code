dfeb352851ac16cd6edf1cd00aa5f352d5281dea
Nguyen Manh Hung <HungNMHE187081@fpt.edu.vn>
2024-10-07 15:38:02 +0700

    test sonarcloud

--- Test_SonarQube/Program.cs
+++ Test_SonarQube/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace SonarCloudTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Example of an unused variable
+            int unusedVariable = 42;
+
+            // Example of a method that could be static
+            Program program = new Program();
+            program.NonStaticMethod();
+
+            // Example of a magic number
+            int result = program.Add(5, 10);
+            Console.WriteLine("Result: " + result);
+        }
+
+        void NonStaticMethod()
+        {
+            Console.WriteLine("This method could be static.");
+        }
+
+        int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
